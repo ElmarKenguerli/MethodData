0593e926aa8f63b36c3f2de7c07010b83ecb3253
ElmarKenguerli <ekenguerli@gmail.com>
2022-11-23 02:13:19 +0200

    Setup working skeleton

---
+++ api_app/src/Pages/ImdbPage.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
+
+export const ImdbPage = () => {
+
+
+    return(
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    )
+
+}
+
+export default ImdbPage;
